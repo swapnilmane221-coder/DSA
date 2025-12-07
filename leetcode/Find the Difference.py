bdb115e1e9ed679c92b40a9b5471eb2718002396
swapnilmane221-coder <swapnilmane2521@gmail.com>
2025-12-07 23:27:25 +0530

    Find the Difference

---
+++ leetcode/Find the Difference.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def findTheDifference(self, s, t):
+        result = 0
+        for char in s:
+            result ^= ord(char)
+        for char in t:
+            result ^= ord(char)
+        return chr(result)
